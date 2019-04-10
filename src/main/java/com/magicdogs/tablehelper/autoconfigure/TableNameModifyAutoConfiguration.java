9dc45b3f4d7d573f7367a2a59ec7410e733c5ab7
magic <hx0011018@hengxinyongli.com>
2019-04-10 11:12:49 +0800

    type: feature   - support mybatis sessionFactory configuration

--- src/main/java/com/magicdogs/tablehelper/autoconfigure/TableNameModifyAutoConfiguration.java
+++ src/main/java/com/magicdogs/tablehelper/autoconfigure/TableNameModifyAutoConfiguration.java
@@ -1,7 +1,7 @@
 package com.magicdogs.tablehelper.autoconfigure;
 
-import com.magicdogs.tablehelper.plugin.ModifyTableNameInterceptor;
 import com.magicdogs.tablehelper.mybatis.InterceptorChainExtend;
+import com.magicdogs.tablehelper.plugin.ModifyTableNameInterceptor;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
@@ -12,21 +12,31 @@ import org.springframework.context.annotation.Configuration;
  * @author magic
  * @date 2019/3/6/006 17:13
  * @version 1.0.0
- * Description ModifyTableNameAutoConfiguration
+ * Description TableNameModifyAutoConfiguration
  */
 @Configuration
-public class ModifyTableNameAutoConfiguration implements ConfigurationCustomizer {
+public class TableNameModifyAutoConfiguration implements ConfigurationCustomizer {
 
     private static final String INTERCEPTOR_CHAIN = "interceptorChain";
 
-    public Interceptor modifyTableNameInterceptor(){
+    public static Interceptor modifyTableNameInterceptor(){
         return new ModifyTableNameInterceptor();
     }
 
     @Override
     public void customize(org.apache.ibatis.session.Configuration configuration) {
+        setConfiguration(configuration);
+    }
+
+    private static void setConfiguration(org.apache.ibatis.session.Configuration configuration) {
         InterceptorChainExtend interceptorChainExtend = new InterceptorChainExtend(modifyTableNameInterceptor());
         MetaObject metaObject = SystemMetaObject.forObject(configuration);
         metaObject.setValue(INTERCEPTOR_CHAIN,interceptorChainExtend);
+    }
+
+    public static org.apache.ibatis.session.Configuration generateConfiguration(){
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        setConfiguration(configuration);
+        return configuration;
     }
 }
